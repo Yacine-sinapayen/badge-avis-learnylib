875aa537e472d7fa9c79f77c420cbdcf71b3a7e6
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2022-06-22 17:06:59 +0200

    julien's version

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+// J'initialise ma note
 const note = 4.5;
+
+// Nombre d'avis
+const reviews = 530;
 
 //  Nombre total d'étoile
 const starsTotal = 5;
@@ -11,9 +15,23 @@ function getRating() {
   const starPercentageRound = `${Math.round(starPercentage / 10) * 10}%`;
 
   // Je définie la largeur de mes étoiles intérieur en pourcentage
-  document.querySelector(`.stars-inner`).style.width = starPercentageRound;
+  var x = document.getElementsByClassName("stars-inner");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.width = starPercentageRound;
+  }
 
-// J'ajoute ma note 
-document.querySelector(`.number-rating`).innerHTML = note
+  // J'ajoute ma note
+  var y = document.getElementsByClassName("number-rating");
+  for (var i = 0; i < y.length; i++) {
+    y[i].innerHTML = `${note}/5`;
+  }
 }
 getRating();
+
+function getReviews() {
+  var r = document.getElementsByClassName("number-reviews");
+  for (var i = 0; i < r.length; i++) {
+    r[i].innerHTML = `${reviews} Avis reçus`;
+  }
+}
+getReviews();
