faaf81b6e0d7e363fe36cb1fda6984414691d0f5
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2022-07-07 14:35:16 +0200

    correction css

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// TODO
 // pop me renvoie le dernière élement du tableau spérarer par split('#')
-const code = window.location.href.split('#').pop();
-const data = atob(code).split(';');
+const code = window.location.href.split("#").pop();
+const data = atob(code).split(";");
 
 const rating = data[0];
 
@@ -10,7 +9,7 @@ const reviews = data[1];
 const type = data[2];
 
 const elem = {
-  wrnd: "<div class='badge round-badge'><span class='number-rating'></span><div class='star-container'><div class='stars-outer'><div class='stars-inner'></div></div></div><p class='number-reviews'></p><div class='container-logo-reviewlib'><img class='img-logo-reviewlib'src='./Assets/logo_reviewlib.svg'alt='Logo-reviewlib'/><p class='p-reviewlib'>REVIEW<strong>LIB</strong></p></div></div>",
+  wrnd: "<div class='badge round-badge'><span class='number-rating'></span><div class='star-container'><div class='stars-outer fa-1x'><div class='stars-inner fa-1x'></div></div></div><p class='number-reviews'></p><div class='container-logo-reviewlib'><p class='p-reviewlib'>REVIEW<strong>LIB</strong></p></div></div>",
 
   wrct: "<div class='badge rectangle-badge'><div class='rectangle-badge-main'><div class='rectangle-badge-left'><div class='star-container rectangle-badge-star-container'><div class='stars-outer'><div class='stars-inner'></div></div></div><p class='number-reviews rectangle-badge-number-reviews'></p><div class='container-logo-reviewlib'> <imgclass='img-logo-reviewlib'src='./Assets/logo_reviewlib.svg'alt='Logo-reviewlib'/><p class='p-reviewlib'>REVIEW<strong>LIB</strong></p></div></div><span class='number-rating rectangle-badge-number-rating'></span></div></div>",
 
@@ -20,38 +19,27 @@ const elem = {
 };
 
 // J'insère mon badge en fonction du params saisi et gestion de l'erreur
-document.getElementById("container-white").innerHTML = elem[type] || elem.wrnd;
-
-// Encoder mes reviews et rating en base64
+// document.getElementById("container-white").innerHTML = elem[type] || elem.wrnd;
+document.getElementById("container").innerHTML = elem.brct;
 
 // Nombre total d'étoile
 const starsTotal = 5;
 
 function getRating() {
-  // Je convertie ma rating en pourcentage
+  // Je convertie ma note en pourcentage
   const starPercentage = (rating / starsTotal) * 100;
 
-  // J'arrondie ma rating à la dizaine la plus proche
+  // J'arrondie ma note à la dizaine la plus proche
   const starPercentageRound = `${Math.round(starPercentage / 10) * 10}%`;
 
   // Je définie la largeur de mes étoiles intérieur en pourcentage
-  var x = document.getElementsByClassName("stars-inner");
-  for (var i = 0; i < x.length; i++) {
-    x[i].style.width = starPercentageRound;
-  }
+  document.getElementsByClassName("stars-inner")[0].style.width =
+    starPercentageRound;
 
-  // J'ajoute ma rating
-  var y = document.getElementsByClassName("number-rating");
-  for (var i = 0; i < y.length; i++) {
-    y[i].innerHTML = `${rating}/5`;
-  }
+  // J'insère ma note
+  document.getElementsByClassName("number-rating")[0].innerHTML = `${rating}/5`;
 }
 getRating();
 
-function getReviews() {
-  var r = document.getElementsByClassName("number-reviews");
-  for (var i = 0; i < r.length; i++) {
-    r[i].innerHTML = `${reviews} Avis reçus`;
-  }
-}
-getReviews();
+// J'insère le nombre d'avis reçus
+document.getElementsByClassName("number-reviews")[0].innerHTML = `${reviews} Avis reçus`;
